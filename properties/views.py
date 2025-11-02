de21aaa326169435433853c730ddd6f0068f4872
alchemist1111 <emoyoeugine@gmail.com>
2025-11-02 14:27:35 +0300

    Task 1: Add Cache Property List View

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.views.decorators.cache import cache_page
+from .models import Property
+from serializers import PropertySerializer
 
-# Create your views here.
+# Cache the property list view for 15 minutes (900 seconds)
+@api_view(['GET'])
+@cache_page(60 * 15)
+def property_list(request):
+    # Retrieve all properties from the database
+    properties = Property.objects.all()
+    serializer = PropertySerializer(properties, many=True)
+    return Response(serializer.data)
